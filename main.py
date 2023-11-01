14595ecb473b0077cfa060223272d3cfcb9537a5
brooklinsantosh <brooklinsantosh@gmail.com>
2023-11-01 14:22:29 +0530

    Data Ingestion completed

---
+++ main.py
@@ -0,0 +1,14 @@
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> {STAGE_NAME} started! <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> {STAGE_NAME} completed! <<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
