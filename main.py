e98c3cdf898d2f36d150ff2eb1d43e7d82a3bde5
brooklinsantosh <brooklinsantosh@gmail.com>
2023-11-07 08:22:30 +0530

    Model Evaluation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textsummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textsummarizer.logging import logger
 
 # Data Ingestion
@@ -46,6 +47,18 @@ try:
     logger.info(f">>>>> {STAGE_NAME} started! <<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
+    logger.info(f">>>>> {STAGE_NAME} completed! <<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+# Model Trainer
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>> {STAGE_NAME} started! <<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>> {STAGE_NAME} completed! <<<<<")
     
 except Exception as e:
